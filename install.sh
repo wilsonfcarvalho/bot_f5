9d00379d2e7d7495fd8a15639e985efc693841e1
wilsonfcarvalho <36923662+wilsonfcarvalho@users.noreply.github.com>
2019-06-14 18:37:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@
 # Artiom Lichtenstein
 # v1.0.1, 24/11/2017
 
-tmsh create ilx workspace SLACK_BOT from-uri https://raw.githubusercontent.com/wilsonfcarvalho/bot_f5/master/bin/bot_f5.tgz > /dev/null 2>&1
+tmsh create ilx workspace ilxws_SLACK_BOT from-uri https://raw.githubusercontent.com/wilsonfcarvalho/bot_f5/master/bin/bot_f5.tgz > /dev/null 2>&1
 read -s -p "Enter Slack API Token: " str_AToken
 echo
-sed -i "s/|| ''/|| '$str_AToken'/" /var/ilx/workspaces/Common/SLACK_BOT/extensions/SLACK_BOT/index.js
-nohup /usr/bin/nodejs /var/ilx/workspaces/Common/SLACK_BOT/extensions/SLACK_BOT/index.js > /var/log/f5-slack-bot &
-
+sed -i "s/|| ''/|| '$str_AToken'/" /var/ilx/workspaces/Common/ilxws_SLACK_BOT/extensions/ilxex_SLACK_BOT/index.js
+nohup /usr/bin/nodejs /var/ilx/workspaces/Common/ilxws_SLACK_BOT/extensions/ilxex_SLACK_BOT/index.js > /var/log/bot_f5 &
